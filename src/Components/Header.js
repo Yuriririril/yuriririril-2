f342bfda67e62cfdfdce31a7b3427d63ee321ccb
Adrian Cabriga <82069846+adriancabriga@users.noreply.github.com>
2022-06-20 15:21:51 +0800

    Add header

---
+++ src/Components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__home">
+        Test
+      </div>
+      <div className="header__options">
+        <div className="header__option">
+          Works
+        </div>
+        <div className="header__option">
+          Bio
+        </div>
+        <div className="header__option">
+          Contacts
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
